ba2ca838ebad118f681baa93db801df0d1ff3c9b
Scott Sum <scottsum@outlook.com>
2019-11-25 22:21:24 -0500

    start script works

---
+++ client/build/precache-manifest.bfd6eb83e5946794c266391de4243405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2413b43f11da0068551309345e755b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07e6ba0f7158a70d1e8",
+    "url": "/static/css/2.4e80ca6c.chunk.css"
+  },
+  {
+    "revision": "a07e6ba0f7158a70d1e8",
+    "url": "/static/js/2.599ded57.chunk.js"
+  },
+  {
+    "revision": "c707870b071387fd484c",
+    "url": "/static/js/main.c7984923.chunk.js"
+  },
+  {
+    "revision": "f4b557619a702a7588a4",
+    "url": "/static/js/runtime-main.eb88ce0c.js"
+  }
+]);
