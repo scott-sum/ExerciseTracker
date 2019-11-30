6e6b07463de79dd2fedf095c7eec1516f8342450
Scott Sum <scottsum@outlook.com>
2019-11-29 22:59:38 -0500

    change server file location

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 //const uri = process.env.ATLAS_URI;
