fc1ff3f252da44b356e95edeac6ab9a7b21581e0
Scott Sum <scottsum@outlook.com>
2019-12-02 20:52:09 -0500

    fix routes

--- server.js
+++ server.js
@@ -54,15 +54,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// importing files
-const routes = require('./routes');
-
 // Define Global Variables
 const app = express();
 const log = console.log;
 const PORT = process.env.PORT || 5000; 
-
-
 
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://scott:mernstackapp1@ds227525.mlab.com:27525/heroku_wgczpk05', {
     useNewUrlParser: true
@@ -71,8 +66,12 @@ mongoose.connect( process.env.MONGODB_URI || 'mongodb://scott:mernstackapp1@ds22
 // Configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', routes);
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
+// whenever go to that url, it will load everything in the second parameter
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
