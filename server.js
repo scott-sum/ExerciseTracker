7a38a8ff0e0459bfdb2c9e810ea66e2511532080
Scott Sum <scottsum@outlook.com>
2019-12-02 21:06:01 -0500

    revert server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*// tools we need
+// tools we need
 const express = require('express');
 const cors = require('cors');
 // mongoose helps connect to mongodb database
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 // setting up server
 const app = express();
 const port = process.env.PORT || 5000;
-
 const path = require("path")
 
 // cors middeware
@@ -15,8 +14,6 @@ app.use(cors());
 
 // helps to parse json
 app.use(express.json());
-
-const connection = mongoose.connection;
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
@@ -47,39 +44,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://scott:mernstackapp1@ds227
 // starts listening and starts the server
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});*/
-// Importing Modules
-const mongoose = require('mongoose');
-const express = require('express');
-const path = require('path');
-
-// Define Global Variables
-const app = express();
-const PORT = process.env.PORT || 5000; 
-
-// helps to parse json
-app.use(express.json());
-
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://scott:mernstackapp1@ds227525.mlab.com:27525/heroku_wgczpk05', {
-    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true 
-});
-
-// Configuration
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
-
-// whenever go to that url, it will load everything in the second parameter
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( 'client/build' ));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-    });
-}
-
-app.listen(PORT, () => {
-    console.log(`Server is starting at PORT: ${PORT}`);
 });
